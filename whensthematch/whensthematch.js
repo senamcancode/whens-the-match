ee078f42ad543914414cc24257169f16c058e373
Senam Daniel <senamdaniel@gmail.com>
2023-04-24 19:58:46 +0200

    changed the checkmatchdate function so that its a pure function

--- whensthematch/whensthematch.js
+++ whensthematch/whensthematch.js
@@ -4,133 +4,133 @@
 checkMatchDate() 
 
 function checkMatchDate() {
-    function liverpoolMatchDay() {
-      let liverpoolFixtureDate = {
-        month: 3,
-        date: 9
-      };
-      let now = new Date();
-      return (now.getMonth() == liverpoolFixtureDate.month && now.getDate() == liverpoolFixtureDate.date);
-    }
-  
-    if (liverpoolMatchDay()) {
+    if (liverpoolMatch()) {
         document.getElementById("liverpool").style.display ='block'; 
         document.getElementById("notplaying").style.display='none'; 
     }
-
-    function westhamMatch() {
-        let westham ={
-            month: 3,
-            date: 16
-        };    
-        let now = new Date();
-        return (now.getMonth() == westham.month && now.getDate() == westham.date);
-        }
         
-        if(westhamMatch()){
-            document.getElementById("westham").style.display ='block'; 
-            document.getElementById("notplaying").style.display='none'; 
+    if(westhamMatch()){
+      document.getElementById("westham").style.display ='block'; 
+      document.getElementById("notplaying").style.display='none'; 
 
     }  
 
-    
-  function southamptonMatch() {
-        let southampton ={
-            month: 3,
-            date: 21
-        }    
-        let now = new Date();
-        return (now.getMonth() == southampton.month && now.getDate() == southampton.date);
-      }
+    if(southamptonMatch()){
+      document.getElementById("southampton").style.display ='block';
+      document.getElementById("notplaying").style.display='none'; 
+    }
 
-      if(southamptonMatch()){
-        document.getElementById("southampton").style.display ='block';
-        document.getElementById("notplaying").style.display='none'; 
-      }
-
-
-  function mancityMatch() {
-        let mancity ={
-            month: 3,
-            date: 26
-        }    
-        let now = new Date();
-        return (now.getMonth() == mancity.month && now.getDate() == mancity.date);
-      }
-
-      if(mancityMatch()){
+    if(mancityMatch()){
         document.getElementById("mancity").style.display ='block';
         document.getElementById("notplaying").style.display='none'; 
-      }
-  function chelseaMatch() {
-        let chelsea ={
-            month: 3,
-            date: 29
-        }    
-        let now = new Date();
-        return (now.getMonth() == chelsea.month && now.getDate() == chelsea.date);
-      }
+    }
 
-      if(chelseaMatch()){
+    if(chelseaMatch()){
         document.getElementById("chelsea").style.display ='block';
         document.getElementById("notplaying").style.display='none'; 
-      }
-  function newcastleMatch() {
-        let newcastle ={
-            month: 4,
-            date: 7
-        }    
-        let now = new Date();
-        return (now.getMonth() == newcastle.month && now.getDate() == newcastle.date);
-      }
+    }
 
-      if(newcastleMatch()){
+    if(newcastleMatch()){
         document.getElementById("newcastle").style.display ='block';
         document.getElementById("notplaying").style.display='none'; 
-      }
-  function brightonMatch() {
-        let brighton ={
-            month: 4,
-            date: 14
-        }    
-        let now = new Date();
-        return (now.getMonth() == brighton.month && now.getDate() == brighton.date);
-      }
-
-      if(brightonMatch()){
+    }
+  
+    if(brightonMatch()){
         document.getElementById("brighton").style.display ='block';
         document.getElementById("notplaying").style.display='none'; 
+    }
       
-  function forrestMatch() {
-        let forrest ={
-            month: 4,
-            date: 20
-        }    
-        let now = new Date();
-        return (now.getMonth() == forrest.month && now.getDate() == forrest.date);
-      }
 
-      if(forrestMatch()){
+    if(forrestMatch()){
         document.getElementById("forrest").style.display ='block';
         document.getElementById("notplaying").style.display='none'; 
-      }
-  function wolvesMatch() {
-        let wolves ={
-            month: 4,
-            date: 28
-        }    
-        let now = new Date();
-        return (now.getMonth() == wolves.month && now.getDate() == wolves.date);
-      }
+    }
 
-      if(wolvesMatch()){
+    if(wolvesMatch()){
         document.getElementById("wolves").style.display ='block';
         document.getElementById("notplaying").style.display='none'; 
-      }
     }
 }
 
+function liverpoolMatch() {
+  let liverpoolFixtureDate = {
+    month: 3,
+    date: 9
+  };
+  let now = new Date();
+  return (now.getMonth() == liverpoolFixtureDate.month && now.getDate() == liverpoolFixtureDate.date);
+}
 
 
+function westhamMatch() {
+  let westham ={
+      month: 3,
+      date: 16
+  };    
+  let now = new Date();
+  return (now.getMonth() == westham.month && now.getDate() == westham.date);
+  }
 
 
+  function southamptonMatch() {
+    let southampton ={
+        month: 3,
+        date: 21
+    }    
+    let now = new Date();
+    return (now.getMonth() == southampton.month && now.getDate() == southampton.date);
+  }
+
+  function mancityMatch() {
+    let mancity ={
+        month: 3,
+        date: 26
+    }    
+    let now = new Date();
+    return (now.getMonth() == mancity.month && now.getDate() == mancity.date);
+  }
+
+  function chelseaMatch() {
+    let chelsea ={
+        month: 3,
+        date: 29
+    }    
+    let now = new Date();
+    return (now.getMonth() == chelsea.month && now.getDate() == chelsea.date);
+  }
+
+  function newcastleMatch() {
+    let newcastle ={
+        month: 4,
+        date: 7
+    }    
+    let now = new Date();
+    return (now.getMonth() == newcastle.month && now.getDate() == newcastle.date);
+  }
+
+  function brightonMatch() {
+    let brighton ={
+        month: 4,
+        date: 14
+    }    
+    let now = new Date();
+    return (now.getMonth() == brighton.month && now.getDate() == brighton.date);
+  }
+
+  function forrestMatch() {
+    let forrest ={
+        month: 4,
+        date: 20
+    }    
+    let now = new Date();
+    return (now.getMonth() == forrest.month && now.getDate() == forrest.date);
+  }
+
+  function wolvesMatch() {
+    let wolves ={
+        month: 4,
+        date: 28
+    }    
+    let now = new Date();
+    return (now.getMonth() == wolves.month && now.getDate() == wolves.date);
+  }
